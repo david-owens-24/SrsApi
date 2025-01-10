8244b6367158d0ead3c0c166a906c136deef9d2d
David Owens <david.owens24@live.com>
2025-01-10 11:40:31 +0000

    added Order to SrsItem

--- SrsApi/DbContext/SrsItem.cs
+++ SrsApi/DbContext/SrsItem.cs
@@ -4,6 +4,11 @@ namespace SrsApi.DbContext
 {
     public class SrsItem : BaseDbModel
     {
+        /// <summary>
+        /// The order in which this SrsItem should appear.
+        /// </summary>
+        public virtual int Order { get; set; }
+
         public virtual SrsItemLevel Level { get; set; }
 
         /// <summary>
---
+++ SrsApi/Migrations/20250110113619_addSrsItemOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SrsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSrsItemOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "SrsItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "SrsItems");
+        }
+    }
+}
