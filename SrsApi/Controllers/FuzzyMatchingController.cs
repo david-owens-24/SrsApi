213aa7a876b8fc977f7460c752cca86c77fc0670
David Owens <david.owens24@live.com>
2025-02-20 14:19:18 +0000

    added FuzzyMatchingController with GET method for FuzzyMatchingService ratio checks

---
+++ SrsApi/Controllers/FuzzyMatchingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SrsApi.Classes.ApiResponses;
+using SrsApi.Classes.SrsItemLevelController;
+using SrsApi.DbContext;
+using SrsApi.Enums;
+using SrsApi.Interfaces;
+
+namespace SrsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FuzzyMatchingController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IBaseServiceWithIncludes<SrsAnswer> _srsAnswerService;
+        private readonly IFuzzyMatchingService _fuzzyMatchingService;
+        private readonly IConfiguration _appsettings;
+
+        public FuzzyMatchingController(ApplicationDbContext context, IBaseServiceWithIncludes<SrsAnswer> srsAnswerService, IFuzzyMatchingService fuzzyMatchingService, IConfiguration appsettings)
+        {
+            _context = context;
+            _srsAnswerService = srsAnswerService;
+            _fuzzyMatchingService = fuzzyMatchingService;
+            _appsettings = appsettings;
+        }
+
+        // GET: api/FuzzyMatching
+        [HttpGet]
+        public async Task<ActionResult<SrsApiResponse>> GetFuzzyMatchRatio(string input, string compare, FuzzySearchMethod fuzzySearchMethod)
+        {
+            var result = _fuzzyMatchingService.GetFuzzyMatchRatio(input, compare, fuzzySearchMethod);
+
+            return SuccessResponse(new { Ratio = result });                     
+        }        
+    }
+}
